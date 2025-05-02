b6c9bf20029de49cf53b7b3fd056467c08500b29
Samuel Walz <sw@noparking.dk>
2025-05-02 14:23:33 +0200

    Refactor and expand block schema structure

--- schemas/block.schema.ts
+++ schemas/block.schema.ts
@@ -1,26 +1,113 @@
 interface RootBlock {
     ['@cls']: '.RootBlock',
     root: BlockElement,
+    /**
+     * file name => file path
+     */
+    mediaFiles: Dictionary<string>,
 }
 
-interface BlockElement {
+
+interface Element {
+    containerData?: ContainerData,
+}
+interface BlockElement extends Element {
 
 }
-interface ParentBlockElement extends BlockElement {
+interface ParentBlock extends BlockElement {
     blocks: BlockElement[],
 }
-interface IControlElement {
+interface TolerantParentBlock extends BlockElement {
+    blocks: Element[],
+}
+interface ControlElement extends Element {
 
 }
 
-interface AttractorBlock extends ParentBlockElement {
+
+
+interface AttractorBlock extends ParentBlock {
     ['@cls']: '.Attractor',
 }
-interface CompositionBlock extends ParentBlockElement {
+interface BookBlock extends ParentBlock {
+    ['@cls']: '.Book',
+}
+interface CameraBlock extends BlockElement {
+    ['@cls']: '.Camera',
+}
+interface CompositionBlock extends TolerantParentBlock {
     ['@cls']: '.Layered',
-
+}
+interface GridBlock extends ParentBlock {
+    ['@cls']: '.Grid',
+}
+interface LiveVideoBlock extends BlockElement {
+    ['@cls']: '.LiveVideo',
 }
 
-interface ButtonControl extends IControlElement {
+
+interface MediaBlock extends BlockElement {
+    ['@cls']: '.MediaBlock',
+}
+interface MediaUrlBlock extends BlockElement {
+    ['@cls']: '.MediaUrl',
+}
+
+/**
+ * The first child element has to be a 2:1 image or video
+ */
+interface PanoramaBlock extends TolerantParentBlock {
+    ['@cls']: '.View360',
+}
+interface ReferenceBlock extends BlockElement {
+    ['@cls']: '.RefBlock',
+}
+
+interface ScrollerBlock extends ParentBlock {
+    ['@cls']: '.Scroller',
+}
+interface SlideShowBlock extends ParentBlock {
+    ['@cls']: '.SlideShow',
+}
+interface StackBlock extends ParentBlock {
+    ['@cls']: '.Stack',
+}
+interface SynchronizerBlock extends ParentBlock {
+    ['@cls']: '.Synchronizer',
+}
+
+interface TagSelectorBlock extends ParentBlock {
+    ['@cls']: '.TagSelector',
+}
+interface TextBlock extends BlockElement {
+    ['@cls']: '.TextBlock',
+}
+interface ThreeDBlock extends BlockElement {
+    ['@cls']: '.ThreeD',
+    blocks: (ButtonControl | MediaBlock)[],
+}
+interface WebBlock extends BlockElement {
+    ['@cls']: '.WebBlock',
+}
+interface WidgetBlock extends BlockElement {
+    ['@cls']: '.Widget',
+}
+
+
+
+interface ButtonControl extends ControlElement {
     ['@cls']: '.Button',
 }
+
+interface ContainerData {
+    kAARect: AARect,
+}
+interface AARect {
+    ['@cls']: '.AARect',
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+}
+
+type Dictionary<T> = { [key: string]: T };
